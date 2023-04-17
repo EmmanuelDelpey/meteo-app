35bcf53705586f647575661781ffd5d24912b34f
EmmanuelDelpey <delpey.emmanuel@gmail.com>
2023-04-18 01:52:06 +0200

    j'en ai marre

--- App.js
+++ App.js
@@ -1,25 +1,37 @@
-import { ImageBackground} from "react-native";
+import { ImageBackground } from "react-native";
 import { Home } from "./pages/Home/Home";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import { s } from './App.style.js';
+import { s } from "./App.style.js";
 import backgroundImg from "./assets/background.png";
 import AlataRegular from "./assets/fonts/Alata-Regular.ttf";
-import{ useFonts } from 'expo-font';
+import { useFonts } from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const [isFontLoaded] = useFonts({
-    "Alata-Regular" : AlataRegular,
+    "Alata-Regular": AlataRegular,
   });
 
   return (
-    <ImageBackground source={backgroundImg} style={s.img_background} imageStyle={s.img}>
-      <SafeAreaProvider>
-        <SafeAreaView style={s.container}>
-          {isFontLoaded ?<Home /> : null}
-        </SafeAreaView>
-      </SafeAreaProvider>
-    </ImageBackground>
-      
-
+    <NavigationContainer>
+      <ImageBackground
+        source={backgroundImg}
+        style={s.img_background}
+        imageStyle={s.img}
+      >
+        <SafeAreaProvider>
+          <SafeAreaView style={s.container}>
+            {isFontLoaded ? (
+              <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={Home} />
+              </Stack.Navigator>
+            ) : null}
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </ImageBackground>
+    </NavigationContainer>
   );
 }
